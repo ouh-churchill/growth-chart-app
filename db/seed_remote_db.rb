e2a10b42a22df75cc6091cd43bdc56abf304c946
Kevin Shekleton <kevin.shekleton@cerner.com>
2015-08-28 02:23:50 -0500

    Update database seed script

--- db/seed_remote_db.rb
+++ db/seed_remote_db.rb
@@ -31,6 +31,7 @@ def post(uri, data)
   request = Net::HTTP::Post.new(uri.request_uri)
   request.body = data
   request['Content-Type'] = 'application/json'
+  request['Accept'] = 'application/json'
 
   response = http.request(request)
   if %w(201 204).include? response.code
@@ -65,22 +66,28 @@ def smart_app(app_id, name, authorized, fhir_server_id, launch_url)
   }.to_json
 end
 
-add_fhir_servers(
-  fhir_server('cerner',      "#{FHIR_SERVER}/dstu2/@tenant_id@"),
-  fhir_server('cerner_open', "#{FHIR_SERVER}/dstu2/open/@tenant_id@")
-)
+# add_fhir_servers(
+#   fhir_server('cerner',      "#{FHIR_SERVER}/may2015/@tenant_id@"),
+#   fhir_server('cerner_open', "#{FHIR_SERVER}/may2015/open/@tenant_id@")
+# )
 
 add_smart_apps(
-  smart_app('bf7d70ce-56a9-4096-bc36-d91dfff00854', 'EnrG|Rheum',              't', 1, 'http://localhost:4271/PointOfEntry'),
-  smart_app('d63f966e-db03-4f4f-9d17-8c604a7b4dd1', 'EnrG|Rheum (No Auth)',    'f', 2, 'http://localhost:4271/PointOfEntry'),
-  smart_app('3175a80b-47b7-471b-aba5-d09349ae7526', 'Cardiac Risk',            't', 1, 'https://fhir.smarthealthit.org/apps/cardiac-risk/launch.html'),
-  smart_app('2dda2e15-9990-4045-b449-00de236b194b', 'BP Percentiles',          't', 1, 'https://fhir.smarthealthit.org/apps/bp-centiles/launch.html'),
-  smart_app('194099bd-080c-48ae-9f0e-5cb1bf559c2a', 'Pediatric Growth Chart',  't', 1, 'https://fhir.smarthealthit.org/apps/growth-chart/launch.html'),
-  smart_app('6bc278b5-c10d-47de-bd19-e3641df492e6', 'Bilirubin Chart',         't', 1, 'http://hspc.isalusconsulting.com/demo/apps/bilirubin-chart/launch.html'),
-  smart_app('428ae1fb-e9ca-4897-8794-206856254c29', 'Charts',                  't', 1, 'org.chip.ios.charts://'),
-  smart_app('df5dd157-c16a-4f9c-b5aa-d87478e9a6d1', 'Crimson Care Management', 't', 1, 'https://fhirdemo.advisory.com/launcher/launch.html'),
-  smart_app('3b9a8fce-a667-40e0-a94a-4aebe4ea1ac1', 'Healthwise',              't', 1, 'https://smart.healthwise.net/initialize'),
-  smart_app('0ec20177-cb96-4217-a8d9-1bdce082c7b4', 'Health Ally',             't', 1, 'https://hspc-portal-conditionplatform-osiamedical.azurewebsites.net/launch.html'),
-  smart_app('6cee11fa-876c-4339-8a40-30ab993cf481', 'Meducation',              't', 1, 'https://fhir.meducation.com/launch.html'),
-  smart_app('1407ed74-394d-4ceb-b2eb-00a0bcd2a557', 'VisualDX',                't', 1, 'http://clupea.visualdx.com/visualdx/widgets/smart/launch.html')
+  # smart_app('bf7d70ce-56a9-4096-bc36-d91dfff00854', 'EnrG|Rheum',              't', 1, 'http://localhost:4271/PointOfEntry'),
+  # smart_app('d63f966e-db03-4f4f-9d17-8c604a7b4dd1', 'EnrG|Rheum (No Auth)',    'f', 2, 'http://localhost:4271/PointOfEntry'),
+  # smart_app('3175a80b-47b7-471b-aba5-d09349ae7526', 'Cardiac Risk',            't', 1, 'https://fhir.smarthealthit.org/apps/cardiac-risk/launch.html'),
+  # smart_app('2dda2e15-9990-4045-b449-00de236b194b', 'BP Percentiles',          't', 1, 'https://fhir.smarthealthit.org/apps/bp-centiles/launch.html'),
+  # smart_app('194099bd-080c-48ae-9f0e-5cb1bf559c2a', 'Pediatric Growth Chart',  't', 1, 'https://fhir.smarthealthit.org/apps/growth-chart/launch.html'),
+  # smart_app('6bc278b5-c10d-47de-bd19-e3641df492e6', 'Bilirubin Chart',         't', 1, 'http://hspc.isalusconsulting.com/demo/apps/bilirubin-chart/launch.html'),
+  # smart_app('428ae1fb-e9ca-4897-8794-206856254c29', 'Charts',                  't', 1, 'org.chip.ios.charts://'),
+  # smart_app('df5dd157-c16a-4f9c-b5aa-d87478e9a6d1', 'Crimson Care Management', 't', 1, 'https://fhirdemo.advisory.com/launcher/launch.html'),
+  # smart_app('3b9a8fce-a667-40e0-a94a-4aebe4ea1ac1', 'Healthwise',              't', 1, 'https://smart.healthwise.net/initialize'),
+  # smart_app('0ec20177-cb96-4217-a8d9-1bdce082c7b4', 'Health Ally',             't', 1, 'https://hspc-portal-conditionplatform-osiamedical.azurewebsites.net/launch.html'),
+  # smart_app('6cee11fa-876c-4339-8a40-30ab993cf481', 'Meducation',              't', 1, 'https://fhir.meducation.com/launch.html'),
+  # smart_app('1407ed74-394d-4ceb-b2eb-00a0bcd2a557', 'VisualDX',                't', 1, 'http://clupea.visualdx.com/visualdx/widgets/smart/launch.html'),
+  # smart_app('a9b3e26a-13c3-4888-82c7-ac130bccae55', 'Qvera Argonaut Client',   't', 1, 'http://mhutton-m4700/form/form.html?urlKey=FHIRClient'),
+  # smart_app('3203fa36-0431-4ddf-add1-c14a343ca386', 'VisualDX (DSTU 2)',       't', 1, 'https://www.visualdx.com/visualdx/widgets/smart/launch.html')
+  # smart_app('b4c2eb41-3252-41a2-9d38-34aad35db8e7', 'MITRE Crucible',          't', 1, 'http://localhost:4200/'),
+  # smart_app('f1ce5e11-ad41-4b57-9afd-ef79f5aeba3b', 'EnrG|Rheum Questionnaire','t', 1, 'http://localhost:4272/SecurePointOfEntry'),
+  # smart_app('2dd93cdf-d9cc-4f42-81c7-cc21f86319ea', 'MAEHC',                   't', 1, 'https://dev.qdc.maehc.org/fhir'),
+  # smart_app('7a259ec0-4afa-40cc-970e-bef0bfcdb581', 'PRP (Premier Inc)',       't', 1, 'http://localhost:8080/fhir-app/PRPHome.html')
 )
