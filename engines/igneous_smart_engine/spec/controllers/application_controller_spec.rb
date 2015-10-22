2d7cfd86c9e6804923f3d5e1fd819d66f73aa311
Thirukonda Surendran,Koushic Babu <KoushicBabu.ThirukondaSurendran@Cerner.com>
2015-10-22 10:12:58 -0500

    Add security headers

--- engines/igneous_smart_engine/spec/controllers/application_controller_spec.rb
+++ engines/igneous_smart_engine/spec/controllers/application_controller_spec.rb
@@ -22,4 +22,15 @@ describe Igneous::Smart::ApplicationController do
       post :index
     end
   end
+
+  describe 'ensure security headers' do
+    it 'verify HTTP security Headers are set' do
+      get :index
+
+      expect(response.headers['X-Frame-Options']).to eq('SAMEORIGIN')
+      expect(response.headers['X-XSS-Protection']).to eq('1; mode=block')
+      expect(response.headers['X-Content-Type-Options']).to eq('nosniff')
+      expect(response.headers['Strict-Transport-Security']).to eq('max-age=631152000')
+    end
+  end
 end
