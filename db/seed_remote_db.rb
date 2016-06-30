8c5ed87062e3c5215d661392170980cd9767ac37
Dennis Patterson <DENNIS.PATTERSON@CERNER.COM>
2016-06-30 12:14:03 -0500

    Updated launch url for MedSafe

--- db/seed_remote_db.rb
+++ db/seed_remote_db.rb
@@ -187,5 +187,5 @@ add_smart_apps(
   # smart_app('96ff31f1-c1df-437f-a8ca-bee0c2657e51', 'Medical Necessity', 't', 4, 'https://www.automatedmd.com/Callback/Cerner/Launch.pl'),
   # smart_app('d0a3d2e1-7d3b-40e9-9b5c-be906a0d7db0', 'CHFPredictiveAnalytics', 't', 4, 'http://223.31.99.62:8000/CHFPredictiveAnalytics/launch.html'),
   # smart_app('75c2941b-0fd1-485b-9375-88209b7f0ddb', 'Clinical Decision Support', 't', 4, 'http://utd03830:8080/acds/launch/ringmaster'),
-  # smart_app('48c70d9d-987d-466e-844e-82365a2f6e1a', 'MedSafe', 't', 4, 'http://localhost:3000/launch.html'),
+  # smart_app('48c70d9d-987d-466e-844e-82365a2f6e1a', 'MedSafe', 't', 4, 'http://gs:3000/launch'),
 )
