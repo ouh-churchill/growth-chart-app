e256591f13d1bb6f07b9792c49beb26cc7b12484
Dennis Patterson <dennis.patterson@cerner.com>
2016-09-08 09:20:34 -0500

    Add ePreop SurgicalValet (Staging) SMART App

--- db/seed_remote_db.rb
+++ db/seed_remote_db.rb
@@ -217,4 +217,5 @@ add_smart_apps(
   # smart_app('bfc562e2-f9e5-4d1b-95c1-f01ca81f64de', 'Cerner SMART App Validator (testing blue/green)', 't', 1, 'https://smart-latest.cernerpowerchart.com/smart-app-validator/launch-latest.html'),
   # smart_app('78b31f8e-51d8-479f-a68d-2a5ae4f7b8d0', 'Cerner SMART App Validator DSTU2 (testing blue/green)', 't', 3, 'https://smart-latest.cernerpowerchart.com/smart-app-validator-2.0/launch-latest.html'),
   # smart_app('6a4c154e-a5ea-47fe-a7e2-dd51626d6640', 'HEART Pathway', 't', 4, 'https://app.decisionpointinformatics.com/smart/launch'),
+  # smart_app('689e2c46-ea7a-4bf4-ab65-6325ad731c78', 'ePreop SurgicalValet (Staging)', 't', 4, 'https://services.staging.epreop.com/ExternalServices/OpenIdConnect10/Launch.aspx'),
 )
