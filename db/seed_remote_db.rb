39cc88c001afdc718b07ae820391677c2b5a66a8
Dennis Patterson <dennis.patterson@cerner.com>
2016-09-29 15:30:33 -0500

    Add HealtheInsights Staging SMART App

--- db/seed_remote_db.rb
+++ db/seed_remote_db.rb
@@ -221,4 +221,5 @@ add_smart_apps(
   # smart_app('4e0974f4-7019-4b23-9341-896862c08024', 'Diabetes Dashboard', 't', 4, 'https://diabetesdashboard.herokuapp.com'),
   # smart_app('d88bd1aa-b138-48fd-9d36-ae219cbf8f2a', 'HealtheInsights Development', 't', 4, 'https://mill-int-test.insights.devhealtheintent.com/smart'),
   # smart_app('4ac08999-67b5-4927-aec7-6d4ba39dc854', 'DHI Cardiac Risk App (Prod)', 't', 4, 'https://dhiapp.ondemand.sas.com/cardiac-risk/launch.html'),
+  # smart_app('7940a6e7-87fe-4e87-89a6-d1f5453ca4f5', 'HealtheInsights Staging', 't', 4, 'https://mill-int-test.insights.staginghealtheintent.com/smart'),
 )
