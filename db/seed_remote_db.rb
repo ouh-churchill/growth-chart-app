e7d2f6f18b6778b49f5da4028323ca2e8579d990
Dennis Patterson <dennis.patterson@cerner.com>
2016-10-04 10:54:34 -0500

    Add 'HealtheInsights Staging (Latest)' SMART App

--- db/seed_remote_db.rb
+++ db/seed_remote_db.rb
@@ -223,4 +223,5 @@ add_smart_apps(
   # smart_app('4ac08999-67b5-4927-aec7-6d4ba39dc854', 'DHI Cardiac Risk App (Prod)', 't', 4, 'https://dhiapp.ondemand.sas.com/cardiac-risk/launch.html'),
   # smart_app('7940a6e7-87fe-4e87-89a6-d1f5453ca4f5', 'HealtheInsights Staging', 't', 4, 'https://mill-int-test.insights.staginghealtheintent.com/smart'),
   # smart_app('a4cbff7f-e747-4294-8f4a-9b487278d332', 'UML_DEMO', 't', 4, 'http://52.54.75.123/launch.html'),
+  # smart_app('27b880c4-cdee-4c51-9454-84be6040cce7', 'HealtheInsights Staging (Latest)', 't', 4, 'https://mill-int-latest.insights.staginghealtheintent.com/smart'),
 )
