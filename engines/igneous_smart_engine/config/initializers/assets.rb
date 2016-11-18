6d1d028005253f99d6fcae533e7ea3faf8e746e0
Matt Henkes <matt.henkes@cerner.com>
2016-11-18 14:59:36 +0100

    OPENSVC-3300 update spinner

--- engines/igneous_smart_engine/config/initializers/assets.rb
+++ engines/igneous_smart_engine/config/initializers/assets.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.assets.precompile += %w( smart_apps.css igneous-smart.min.js application.js )
+Rails.application.config.assets.precompile += %w( spinner.css smart_apps.css igneous-smart.min.js application.js )
