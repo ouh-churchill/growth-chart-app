57fdba5ed6d329dc3737bb13843ba81d08f020eb
Zach Plata <Zach.Plata@Cerner.com>
2017-05-01 17:28:46 -0500

    OPENSVC-4128 Add loading spinner to ASCVD launch sequence (#266)

--- public/ascvd-risk-app/index.jsx
+++ public/ascvd-risk-app/index.jsx
@@ -5,6 +5,11 @@ import ASCVDRisk from './app/load_fhir_data';
 
 ASCVDRisk.fetchPatientData().then(
   () => {
+    const spinnerNode = document.getElementById('loadingSpinner');
+    while (spinnerNode.firstChild) {
+      spinnerNode.removeChild(spinnerNode.firstChild);
+    }
+    spinnerNode.parentNode.removeChild(spinnerNode);
     ReactDOM.render(<App />, document.getElementById('container'));
   },
 );
