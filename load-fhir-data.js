5cb15553a251f534ffb72b7cada5399952500692
Shriniket Sarkar <shriniketsarkar@hotmail.com>
2017-12-02 11:31:22 -0600

    Added more LOINC’s to the app to request with the Observation’s Resource call inorder to improve on the chances of getting observation’s for weight, height, bone age, gestational age, parental heights. (#20)
    Added more LOINC’s to the app to request with the Observation’s Resource call inorder to improve on the chances of getting observation’s for weight, height, bone age, gestational age, parental heights. (#20)

--- load-fhir-data.js
+++ load-fhir-data.js
@@ -75,7 +75,7 @@ GC.get_data = function() {
                 onErrorWithWarning(GC.str('STR_Error_UnknownGender'));
             }
 
-            var vitalsByCode = smart.byCode(vitals, 'code');
+            var vitalsByCode = smart.byCodes(vitals, 'code');
 
             // Initialize an empty patient structure
             var p = {
@@ -128,10 +128,10 @@ GC.get_data = function() {
             p.demographics.birthday = patient.birthDate;
             p.demographics.gender = patient.gender;
 
-            var gestAge = vitalsByCode['18185-9'];
-            if (gestAge === undefined) {
+            var gestAge = vitalsByCode('18185-9');
+            if (gestAge === undefined || gestAge.length == 0) {
                 //handle an alternate mapping of Gest Age used by Cerner
-                gestAge = vitalsByCode['11884-4'];
+                gestAge = vitalsByCode('11884-4');
             }
 
             if (gestAge && gestAge.length > 0) {
@@ -163,16 +163,15 @@ GC.get_data = function() {
             }
 
             var units = smart.units;
-            process(vitalsByCode['3141-9'], units.kg, p.vitals.weightData);
-            process(vitalsByCode['8302-2'],  units.cm,  p.vitals.lengthData);
-            process(vitalsByCode['8287-5'],  units.cm,  p.vitals.headCData);
-            process(vitalsByCode['39156-5'], units.any, p.vitals.BMIData);
+
+            process(vitalsByCode('3141-9','29463-7','8335-2'), units.kg, p.vitals.weightData);
+            process(vitalsByCode('8302-2','8301-4','3137-7'),  units.cm,  p.vitals.lengthData);
+            process(vitalsByCode('8287-5'),  units.cm,  p.vitals.headCData);
+            process(vitalsByCode('39156-5'), units.any, p.vitals.BMIData);
             //Bone Age: The reason to prefer the LOINC 85151-9 over 37362-1 is because 37362-1 is a LOINC for radiology report (document)
             // and may not represent a quantitative value which the app needs.
-            var boneAgeObservations = vitalsByCode['85151-9'] ? vitalsByCode['85151-9'] : vitalsByCode['37362-1'];
+            var boneAgeObservations = vitalsByCode('85151-9') ? vitalsByCode('85151-9') : vitalsByCode('37362-1');
             processBoneAge(boneAgeObservations, p.boneAge, units);
-
-
 
             $.each(familyHistories, function(index, fh) {
                 if (fh.resourceType === "FamilyMemberHistory") {
@@ -231,16 +230,34 @@ GC.get_data = function() {
                     query: {
                         code: {
                             $or: [
+                                //Weight
                                 'http://loinc.org|3141-9',
+                                'http://loinc.org|29463-7',
+                                'http://loinc.org|8335-2',
+
+                                //Height
                                 'http://loinc.org|8302-2',
+                                'http://loinc.org|8301-4',
+                                'http://loinc.org|3137-7',
+
+                                //Head Circumference
                                 'http://loinc.org|8287-5',
+
+                                //BMI
                                 'http://loinc.org|39156-5',
-                                'http://loinc.org|18185-9',
+
+                                //Bone Age
+                                'http://loinc.org|85151-9',
                                 'http://loinc.org|37362-1',
+
+                                //Gestsational Age
+                                'http://loinc.org|18185-9',
                                 'http://loinc.org|11884-4',
+
+                                //Parental Height Father
                                 'http://loinc.org|83845-8',
-                                'http://loinc.org|83846-6',
-                                'http://loinc.org|85151-9'
+                                //Parental Height Mother
+                                'http://loinc.org|83846-6'
                             ]
                         }
                     }
